3239302548b47eb0a9416f00b983d555ae1fa6b7
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-05-20 17:26:17 -0300

    Criado o teste do metodo de cadastrar cidade.

---
+++ src/test/java/com/projeto_final/PrevisaoDoTempo/fixture/CidadeRequestDtoFixture.java
@@ -0,0 +1,27 @@
+package com.projeto_final.PrevisaoDoTempo.fixture;
+
+import com.projeto_final.PrevisaoDoTempo.core.dto.CidadeRequestDdo;
+import com.projeto_final.PrevisaoDoTempo.core.entities.DadoMeteorologico;
+import com.projeto_final.PrevisaoDoTempo.core.enuns.Clima;
+import com.projeto_final.PrevisaoDoTempo.core.enuns.Turno;
+
+import java.time.LocalDate;
+
+public class CidadeRequestDtoFixture {
+
+    public static CidadeRequestDdo gerarCidadeRequestDto(){
+        DadoMeteorologico dadoMeteorologico = new DadoMeteorologico(
+                null,
+                LocalDate.now(),
+                20,
+                10,
+                Turno.NOITE,
+                Clima.NUBLADO,
+                50
+                );
+
+
+        return new CidadeRequestDdo("Campinas", dadoMeteorologico);
+
+    }
+}
