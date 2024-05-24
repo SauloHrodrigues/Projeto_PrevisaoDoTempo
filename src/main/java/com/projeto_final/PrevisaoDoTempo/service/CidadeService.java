08e3adfef5ef997df9397d2b8aef1dc93812ec19
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-05-24 16:48:37 -0300

    Refatorado a Classe CidadeService, bem como incuida a clase AuxiliarService, que a extende.

--- src/main/java/com/projeto_final/PrevisaoDoTempo/service/CidadeService.java
+++ src/main/java/com/projeto_final/PrevisaoDoTempo/service/CidadeService.java
@@ -2,53 +2,49 @@ package com.projeto_final.PrevisaoDoTempo.service;
 
 import com.projeto_final.PrevisaoDoTempo.core.dto.CidadeRequestDdo;
 import com.projeto_final.PrevisaoDoTempo.core.dto.CidadeResponseDto;
-import com.projeto_final.PrevisaoDoTempo.core.dto.DadoMeteorologicoResponseDto;
+import com.projeto_final.PrevisaoDoTempo.core.dto.DadoMeteorologicoRequestDto;
 import com.projeto_final.PrevisaoDoTempo.core.entities.Cidade;
 import com.projeto_final.PrevisaoDoTempo.core.entities.DadoMeteorologico;
 import com.projeto_final.PrevisaoDoTempo.repositories.CidadeRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-public class CidadeService {
-
+public class CidadeService  extends AuxiliarService {
 
     private final CidadeRepository cidadeRepository;
 
-
-
-    public Cidade cadastrarCidade(CidadeRequestDdo cidadeRequestDdo){
-        Cidade novaCidade = new Cidade();
-        DadoMeteorologico dadoMeteorologico = new DadoMeteorologico();
-        BeanUtils.copyProperties(cidadeRequestDdo, novaCidade);
-        BeanUtils.copyProperties(cidadeRequestDdo.getDadosMeteorologicos(), dadoMeteorologico);
-        novaCidade.getDadosMeteorologicos().put(cidadeRequestDdo.getDadosMeteorologicos().getData(), cidadeRequestDdo.getDadosMeteorologicos());
-        return cidadeRepository.save(novaCidade);
-
+    public void cadastrarCidade(CidadeRequestDdo cidadeRequestDdo) {
+        Cidade novaCidade = criarNovaCidade(cidadeRequestDdo);
+        cidadeRepository.save(novaCidade);
     }
 
-    public List<Cidade> listar(){
+    public List<Cidade> listar() {
         List<Cidade> response = cidadeRepository.findAll();
         return response;
     }
 
-    public CidadeResponseDto retornaDadosMeteorologicoPorCidade(String nomeDaCidade){
-        Cidade  cidadePesquisada = cidadeRepository.findByNome(nomeDaCidade).orElseThrow(() -> new NoSuchElementException("Cidade não encontrada"));
-        CidadeResponseDto cidadeResponseDto = new CidadeResponseDto();
-        BeanUtils.copyProperties(cidadePesquisada,cidadeResponseDto);
+    public CidadeResponseDto retornaDadosMeteorologicoPorCidade(String nomeDaCidade) {
+        Cidade cidadePesquisada = pesquisarCidadePorNome(nomeDaCidade);
+        CidadeResponseDto cidadeResponseDto = convertCidadeToResponseDto(cidadePesquisada);
         return cidadeResponseDto;
-
     }
 
+    public CidadeResponseDto cadastrarDadosMeteorologicos(DadoMeteorologicoRequestDto dadoMeteorologico, String cidade) {
+        Cidade cidadeEntity = pesquisarCidadePorNome(cidade);
+        DadoMeteorologico novoDadoMeteorologico = criarNovoDadoMeteirologico(dadoMeteorologico);
+        cidadeEntity.getDadosMeteorologicos().add(novoDadoMeteorologico);
+        cidadeRepository.save(cidadeEntity);
+        CidadeResponseDto cidadeAtualizada = convertCidadeToResponseDto(cidadeEntity);
+        return cidadeAtualizada;
+    }
 
+    private Cidade pesquisarCidadePorNome(String cidade) {
+        return cidadeRepository.findByNome(cidade).orElseThrow(() -> new NoSuchElementException("Cidade não encontrada"));
+    }
 
 }
