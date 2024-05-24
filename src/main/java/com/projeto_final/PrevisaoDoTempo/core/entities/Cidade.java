1f5ba6f5c0ed9f6cf1f0b865e71c068bb0ee5128
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-05-24 16:32:27 -0300

    Alterado a visibilidades dos atributos da classe cidade.

--- src/main/java/com/projeto_final/PrevisaoDoTempo/core/entities/Cidade.java
+++ src/main/java/com/projeto_final/PrevisaoDoTempo/core/entities/Cidade.java
@@ -2,18 +2,25 @@ package com.projeto_final.PrevisaoDoTempo.core.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Entity
 @Table(name = "cidades")
 public class Cidade {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
+    @Getter
+    @Setter
     private String nome;
+    @Getter
+    @Setter
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "cidade")
     private List<DadoMeteorologico> dadosMeteorologicos= new ArrayList<>();
