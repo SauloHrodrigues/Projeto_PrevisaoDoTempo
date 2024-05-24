242c95e44225574c5ca9bc6040cc7151ce03f5b5
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-05-24 16:52:07 -0300

    Refatorado toda a classe CidadeController.

--- src/main/java/com/projeto_final/PrevisaoDoTempo/webservice/CidadeController.java
+++ src/main/java/com/projeto_final/PrevisaoDoTempo/webservice/CidadeController.java
@@ -2,34 +2,43 @@ package com.projeto_final.PrevisaoDoTempo.webservice;
 
 import com.projeto_final.PrevisaoDoTempo.core.dto.CidadeRequestDdo;
 import com.projeto_final.PrevisaoDoTempo.core.dto.CidadeResponseDto;
+import com.projeto_final.PrevisaoDoTempo.core.dto.DadoMeteorologicoRequestDto;
+import com.projeto_final.PrevisaoDoTempo.core.dto.DadoMeteorologicoResponseDto;
 import com.projeto_final.PrevisaoDoTempo.core.entities.Cidade;
+import com.projeto_final.PrevisaoDoTempo.core.entities.DadoMeteorologico;
 import com.projeto_final.PrevisaoDoTempo.service.CidadeService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @AllArgsConstructor
 @RestController
-@RequestMapping(value = "/cidades")
+@RequestMapping(value = "/previsao/clima/cidade")
 public class CidadeController {
 
     private final CidadeService cidadeService;
 
-    @GetMapping("/listar")
+    @PostMapping("/")
+    public void cadastraCidade(@RequestBody CidadeRequestDdo requisicao ){
+        cidadeService.cadastrarCidade(requisicao);
+    }
+
+    @GetMapping("/")
     public List<Cidade> lista(){
         return cidadeService.listar();
     }
 
-    @PostMapping("/cadastrar")
-    public void cadastraCidade(@RequestBody CidadeRequestDdo requisicao ){
-        cidadeService.cadastrarCidade(requisicao);
-
-    }
-
     @GetMapping("/{cidade}")
     public CidadeResponseDto pesquisarCidade(@PathVariable("cidade") String nomeCidade){
-        System.out.println("Nome Cidade = "+ nomeCidade);
         return cidadeService.retornaDadosMeteorologicoPorCidade(nomeCidade);
+    }
+
+    @PostMapping("/novo_dado/{cidade}")
+    public ResponseEntity<CidadeResponseDto> cadastrarDadosMeteorologicos(@RequestBody DadoMeteorologicoRequestDto dados, @PathVariable("cidade")  String cidade ){
+        CidadeResponseDto response = cidadeService.cadastrarDadosMeteorologicos(dados,cidade);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
