ca0bfbdea6f731626777f6da8dc231ad69024694
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-05-24 16:36:54 -0300

    Alterado a visibilidades dos atributos da classe DadoMeteorologico.

--- src/main/java/com/projeto_final/PrevisaoDoTempo/core/entities/DadoMeteorologico.java
+++ src/main/java/com/projeto_final/PrevisaoDoTempo/core/entities/DadoMeteorologico.java
@@ -1,29 +1,36 @@
 package com.projeto_final.PrevisaoDoTempo.core.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.projeto_final.PrevisaoDoTempo.core.enuns.Clima;
 import com.projeto_final.PrevisaoDoTempo.core.enuns.Turno;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-@Data
+
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-//@Table(name = "dados_meteorologico")
+@Table(name = "dados_meteorologico")
 public class DadoMeteorologico {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_dados")
     private Long id;
+    @Getter @Setter
     private LocalDate data;
+    @Getter @Setter
     private Integer temperaturaMinima;
+    @Getter @Setter
     private Integer temperaturaMaxima;
+    @Getter @Setter
     @Enumerated(EnumType.STRING)
     private Turno turno;
+    @Getter @Setter
     @Enumerated(EnumType.STRING)
     private Clima clima;
+    @Getter @Setter
     private Integer precipitacao;
 }
