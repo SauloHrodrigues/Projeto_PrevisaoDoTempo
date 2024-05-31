cc2e774d1fe382f3df62c7270f3a46a1709bc3f5
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-05-31 11:45:05 -0300

    Realizado modificações nas classes Fixtures.

--- src/test/java/com/projeto_final/PrevisaoDoTempo/fixture/CidadeRequestDtoFixture.java
+++ src/test/java/com/projeto_final/PrevisaoDoTempo/fixture/CidadeRequestDtoFixture.java
@@ -7,19 +7,5 @@ import com.projeto_final.PrevisaoDoTempo.mapper.MapperDadosMetearologicos;
 
 public class CidadeRequestDtoFixture {
 
-    public static CidadeRequestDdo gerarCidadeRequestDto(String nomeDaCidade){
-        CidadeRequestDdo cidadeDto = new CidadeRequestDdo();
-        DadoMeteorologico dadoMeteorologico = DadoMeteorologicoFixture.gerarDadoMeteorologico();
-        DadoMeteorologicoResponseDto reponse = MapperDadosMetearologicos.entityToResponseDto(dadoMeteorologico);
-        cidadeDto.setNome(nomeDaCidade);
-        return cidadeDto;
-    }
-    public static CidadeRequestDdo  gerarCidadeRequestDtoComDadosMeteorologico(String nomeDaCidade){
-        CidadeRequestDdo cidadeDto = new CidadeRequestDdo();
-        DadoMeteorologico dadoMeteorologico = DadoMeteorologicoFixture.gerarDadoMeteorologico();
-        DadoMeteorologicoResponseDto reponse = MapperDadosMetearologicos.entityToResponseDto(dadoMeteorologico);
-        cidadeDto.setNome(nomeDaCidade);
-        cidadeDto.setDadosMeteorologicos(reponse);
-        return cidadeDto;
-    }
+
 }
